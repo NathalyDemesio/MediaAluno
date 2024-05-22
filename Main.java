cf45af43c8a4e040e9c5e815cea88a37b2550991
Nathy <163008302+NathalyDemesio@users.noreply.github.com>
2024-05-22 09:45:31 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,17 +2,21 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
+    //INICIALIZAÇÃO DE VARIÁVEIS
     int nota = 0;
     float mediaAritmetica = 0.0f;
     float mediaPonderada = 0.0f;
     int media = 0;
 
+    //CAPTURA DE DADOS DO TECLADO
     Scanner entrada = new Scanner(System.in);
 
+    //ESCOLHA DE MÉDIA
     System.out.print("Qual média você quer? (mediaArit = 1) (mediaPond = 2): ");
     media = entrada.nextInt();
     
 
+    //ESTRUTURA DE REPETIÇÃO FOR PARA LEITURA DO TECLADO E ADIÇÃO NAS MÉDIAS
     for (int i = 1; i <= 4; i++) {
 
       System.out.print("Digite a nota " + i + ": ");
@@ -31,9 +35,11 @@ public class Main {
     }
     
 
+    //CALCULAR MÉDIAS
     mediaAritmetica /= 4;
     mediaPonderada /= 10;
 
+    //ESTRUTURA DE DECISÃO PARA MOSTRAR RESPOSTA
     if (media == 1) {
       System.out.println("A média aritmética é " + mediaAritmetica);
 
